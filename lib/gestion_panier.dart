a18cf799bdc9a232e931c337c50fa72c9ec3a070
DjaberBenyezza <djaberbenyezza0041@gmail.com>
2025-11-25 15:49:01 -0500

    Mise à jour 3 gestion_panier.dart
    Calcul du total du panier

--- lib/gestion_panier.dart
+++ lib/gestion_panier.dart
@@ -8,3 +8,13 @@ class GestionPanier {
   static void supprimerProduit(int index) {
     panier.removeAt(index);
   }
+
+  static double getTotal() {
+    double total = 0;
+
+    for (var item in panier) {
+      total += item["prix"];
+    }
+
+    return total;
+  }
