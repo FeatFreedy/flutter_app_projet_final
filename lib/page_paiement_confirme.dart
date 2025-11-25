67747d0e9e04e2a719f9b1e756847ed63dd43c1e
AwaWORKS <awaodendod@gmail.com>
2025-11-25 11:01:29 -0800

    Structure initiale de PagePaiementConfirme

--- lib/page_paiement_confirme.dart
+++ lib/page_paiement_confirme.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
 
+class PagePaiementConfirme extends StatelessWidget {
+  const PagePaiementConfirme({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [],
+        ),
+      ),
+    );
+  }
+}
