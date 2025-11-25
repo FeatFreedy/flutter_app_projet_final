842d542decf5405b1966064b39230e9e93e6e74f
Freedy Ebah <freedy.ebah@yahoo.com>
2025-11-25 14:56:37 -0500

    fichier main
    Mise a jour du fichier main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
+import 'page_connexion.dart';
+import 'gestion_utilisateurs.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GestionUtilisateur.chargerUtilisateurs();
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: PageConnexion(),
     );
   }
 }
