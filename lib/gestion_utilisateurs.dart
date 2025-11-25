6b91ca3d5323a52aebd9c99eed46ad953311f909
AwaWORKS <awaodendod@gmail.com>
2025-11-25 11:09:27 -0800

    gestion_utilisateurs_finale

--- lib/gestion_utilisateurs.dart
+++ lib/gestion_utilisateurs.dart
@@ -1,1 +1,22 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
 
+class GestionUtilisateur {
+  static List utilisateurs = [];
+  static Map<String, dynamic>? utilisateurConnecte;
+
+  static Future<void> chargerUtilisateurs() async {
+    String data = await rootBundle.loadString('assets/data/utilisateurs.json');
+    utilisateurs = jsonDecode(data);
+  }
+
+  static bool verifierConnexion(String email, String password) {
+    for (var user in utilisateurs) {
+      if (user["email"] == email && user["password"] == password) {
+        utilisateurConnecte = user;
+        return true;
+      }
+    }
+    return false;
+  }
+}
