17a4fc45d6117a3d6786fe4bed515191e7f770a3
DjaberBenyezza <djaberbenyezza0041@gmail.com>
2025-11-25 15:55:39 -0500

    Mise à jour 3 page_panier.dart
    afficher la liste des produits du panier

--- lib/page_panier.dart
+++ lib/page_panier.dart
@@ -19,7 +19,25 @@ class _PagePanierState extends State<PagePanier> {
       ),
       body: panier.isEmpty
           ? const Center(child: Text("Votre panier est vide."))
-          : const Center(child: Text("Panier détecté")),
+          : ListView.builder(
+              itemCount: panier.length,
+              itemBuilder: (context, index) {
+                final produit = panier[index];
+
+                return Card(
+                  margin: const EdgeInsets.all(10),
+                  child: ListTile(
+                    leading: Image.asset(
+                      produit["image"],
+                      width: 60,
+                      fit: BoxFit.cover,
+                    ),
+                    title: Text(produit["titre"]),
+                    subtitle: Text("\$${produit["prix"]}"),
+                  ),
+                );
+              },
+            ),
     );
   }
 }
