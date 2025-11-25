0eb255ae428fbbf18732c8c0208c976225fd090e
AwaWORKS <awaodendod@gmail.com>
2025-11-25 11:03:09 -0800

    Ajout de l’interface utilisateur icône et textes

--- lib/page_paiement_confirme.dart
+++ lib/page_paiement_confirme.dart
@@ -7,10 +7,46 @@ class PagePaiementConfirme extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [],
+          children: [
+
+            Container(
+              padding: const EdgeInsets.all(25),
+              decoration: BoxDecoration(
+                color: Colors.green.shade100,
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                Icons.check,
+                size: 90,
+                color: Colors.green.shade700,
+              ),
+            ),
+
+            const SizedBox(height: 30),
+
+            const Text(
+              "Paiement confirmé",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            const Text(
+              "Votre commande a été traitée\navec succès.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 17,
+                color: Colors.grey,
+              ),
+            ),
+          ],
         ),
       ),
     );
