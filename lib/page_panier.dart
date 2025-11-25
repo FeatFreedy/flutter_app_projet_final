49d1a0f7965f4c8809ed16ccb32db44a47b94965
DjaberBenyezza <djaberbenyezza0041@gmail.com>
2025-11-25 15:53:26 -0500

    Mise à jour 1 page_panier.dart

--- lib/page_panier.dart
+++ lib/page_panier.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+
+class PagePanier extends StatefulWidget {
+  const PagePanier({super.key});
+
+  @override
+  State<PagePanier> createState() => _PagePanierState();
+}
+
+class _PagePanierState extends State<PagePanier> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Mon Panier"),
+      ),
+      body: const Center(
+        child: Text("Chargement du panier..."),
+      ),
+    );
+  }
+}
 
