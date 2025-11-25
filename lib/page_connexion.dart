c56b89322a90a95802d758b8af0c001d3ac4b87f
AwaWORKS <awaodendod@gmail.com>
2025-11-25 10:43:01 -0800

    Create de la page_connexion

---
+++ lib/page_connexion.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class PageConnexion extends StatefulWidget {
+  const PageConnexion({super.key});
+
+  @override
+  State<PageConnexion> createState() => _PageConnexionState();
+}
+
+class _PageConnexionState extends State<PageConnexion> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            SizedBox(height: 40),
+          ],
+        ),
+      ),
+    );
+  }
+}
